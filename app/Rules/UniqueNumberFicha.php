275ba9fa4ca116e3a6f3540e2753fa641dca4ad1
Daniel Vega <daniel.vega03@hotmail.com>
2024-09-25 20:55:12 -0500

    Subiendo archivo Rule Ficha

---
+++ app/Rules/UniqueNumberFicha.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class UniqueNumberFicha implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $numero = request("numero") ?? 0;
+        if ($numero == 0) {
+            return !\App\Models\Ficha::where('numero', $value)->exists();
+        }
+        if ($numero == $value) {
+            return true;
+        }
+        return !\App\Models\Ficha::where('numero', $value)->exists();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Número de ficha ya existe.';
+    }
+}

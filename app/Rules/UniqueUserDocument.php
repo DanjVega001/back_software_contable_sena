ff95bd81ed4e59771b3edbba72ed33d0b04385c1
Daniel Vega <daniel.vega03@hotmail.com>
2024-09-26 18:41:19 -0500

    Rule de unico documento del usuario subiendo

---
+++ app/Rules/UniqueUserDocument.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class UniqueUserDocument implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    private $user_id;
+
+    public function __construct(int $user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return !\App\Models\User::where('numero_identificacion', $value)->exists() ||
+            \App\Models\User::find($this->user_id)->numero_identificacion == $value;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Número de documento ya usado';
+    }
+}
